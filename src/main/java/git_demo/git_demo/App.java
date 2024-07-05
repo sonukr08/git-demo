2e603a653cf6c4d618645fb4e80d8757211393e9
sonu <sonu0829>
2024-07-05 18:05:17 +0530

    addedd if condition

--- src/main/java/git_demo/git_demo/App.java
+++ src/main/java/git_demo/git_demo/App.java
@@ -6,6 +6,12 @@ package git_demo.git_demo;
  */
 public class App 
 {
+	
+	public void addData(String data)
+	{
+		System.out.println("data"+data);
+		System.out.println("sonu");
+		}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
